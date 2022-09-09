6bff41d090eac8dd27860683da8694de15e14b0e
Minsoo Kim <min-soo.kim@outlook.kr>
2022-09-10 01:15:15 +0900

    fix bug due to typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 install_requires = [
     "accelerate==0.12.0",
     "diffusers==0.3.0",
-    "ftfy==6.1.1"
+    "ftfy==6.1.1",
     "gradio==3.2",
     "torch==1.12.0",
     "torchvision==0.13.0",
