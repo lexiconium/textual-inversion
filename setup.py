17c3f2683c4c3a9486e8ed981801b30e34434ccf
Minsoo Kim <min-soo.kim@outlook.kr>
2022-09-10 01:19:49 +0900

    remove unnecessaries

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@ from setuptools import find_packages, setup
 install_requires = [
     "accelerate==0.12.0",
     "diffusers==0.3.0",
-    "ftfy==6.1.1",
+    "transformers==4.21.1",
     "gradio==3.2",
-    "torch==1.12.0",
-    "torchvision==0.13.0",
-    "tqdm",
-    "transformers==4.21.1"
+    "ftfy==6.1.1"
 ]
 
 setup(
